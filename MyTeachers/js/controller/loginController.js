00a61f8e30c744888ad644defc6f06b6a9ddc508
amit kumar <kumaramit.sahoo@live.com>
2016-04-21 17:24:08 +0530

    First Release for practice

---
+++ MyTeachers/js/controller/loginController.js
@@ -0,0 +1,16 @@
+﻿app.controller('loginController', function ($scope) {
+    //This will hide the DIV by default.
+    $scope.IsVisible = false;
+    $scope.IsVisibleReg = false;
+    //This will show the DIV after button click.
+    $scope.showLoginDiv = function () {
+        $scope.IsVisible = true;
+        console.log("My:");
+        
+    }
+    $scope.showRegisterDiv = function () {
+        $scope.IsVisibleReg = true;
+        console.log("My:");
+        
+    }
+});
